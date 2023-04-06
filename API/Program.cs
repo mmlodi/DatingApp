fb5a1573278b43900930d01e6b8cc016ba43745e
Mateus Lodi <mmischel@hotmail.com>
2023-04-05 22:23:19 -0300

    criacao de um novo app

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 //Configure the http request pipeline
- app.MapControllers();
+app.UseCors( builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.MapControllers();
 
 app.Run();
